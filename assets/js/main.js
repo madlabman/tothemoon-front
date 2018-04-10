ee1ddd815d29e30a431478a08f6d02bd52796157
chtotonetak <madlabman@mail.ru>
2018-04-10 10:52:31 +0400

    Added reply slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,15 @@
 
 let buttons = Array.from(document.getElementsByClassName('button_grad-border'));
 buttons.forEach(function (elem) {
-	elem.innerHTML += "<svg xmlns='http://www.w3.org/2000/svg' viewBox='1 1 152 40' preserveAspectRatio='none'>\n" +
-		"    <defs>\n" +
-		"      <linearGradient id='border-gradient' gradientUnits='objectBoundingBox' gradientTransform='rotate(68)'>\n" +
-		"        <stop offset='0%' stop-color='rgb(0,255,242)' />\n" +
-		"        <stop offset='100%' stop-color='rgb(15,163,149)' />\n" +
-		"      </linearGradient>\n" +
-		"    </defs>\n" +
-		"    <path d='M5.000,2.000 L149.000,2.000 C150.657,2.000 152.000,3.343 152.000,5.000 L152.000,37.000 C152.000,38.657 150.657,40.000 149.000,40.000 L5.000,40.000 C3.343,40.000 2.000,38.657 2.000,37.000 L2.000,5.000 C2.000,3.343 3.343,2.000 5.000,2.000 Z' />\n" +
-		"  </svg>";
+    elem.innerHTML += "<svg xmlns='http://www.w3.org/2000/svg' viewBox='1 1 152 40' preserveAspectRatio='none'>\n" +
+        "    <defs>\n" +
+        "      <linearGradient id='border-gradient' gradientUnits='objectBoundingBox' gradientTransform='rotate(68)'>\n" +
+        "        <stop offset='0%' stop-color='rgb(0,255,242)' />\n" +
+        "        <stop offset='100%' stop-color='rgb(15,163,149)' />\n" +
+        "      </linearGradient>\n" +
+        "    </defs>\n" +
+        "    <path d='M5.000,2.000 L149.000,2.000 C150.657,2.000 152.000,3.343 152.000,5.000 L152.000,37.000 C152.000,38.657 150.657,40.000 149.000,40.000 L5.000,40.000 C3.343,40.000 2.000,38.657 2.000,37.000 L2.000,5.000 C2.000,3.343 3.343,2.000 5.000,2.000 Z' />\n" +
+        "  </svg>";
 });
 
 // const spy = new ScrollSpy('body', {
@@ -35,7 +35,6 @@ $(document).ready(function() {
                 min: target.offset().top - 65,
                 max: target.offset().top + target.height() - 75,
                 onEnter: function(element, position) {
-                    if(console) console.log('entering ' +  element.id);
                     self.parent().addClass('side-nav__scrollspy__item_active');
                 },
                 onLeave: function(element, position) {
@@ -43,5 +42,20 @@ $(document).ready(function() {
                 }
             });
         }
+    });
+
+    let swiper = new Swiper('.swiper-reply', {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 });
---
+++ readme.md
@@ -0,0 +1,10 @@
+#Посадочная страница фонда
+##Демо
+[tothemoon](http://fund.chtotonetak.ru)
+##Cборка
+```bash
+yarn
+bower install
+gulp build
+```
+__!__ Не забывать проверять `bower_components/<component>/.bower.json` на наличие директивы `main`
